c41067eae6929f4348ea31ff6e2ddaaf2e75e4f3
Fahad Mehmood <mehmoodfahad866@gmail.com>
2025-10-20 17:34:22 +0500

    Completed Working On Plan Holiday

---
+++ Plan Holidays/vertical-thumbnail-slider-slick-slider/README.md
@@ -0,0 +1,6 @@
+# Vertical Thumbnail Slider | Slick Slider
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/michael-lewallen/pen/jedLqG](https://codepen.io/michael-lewallen/pen/jedLqG).
+
---
+++ Plan Holidays/vertical-thumbnail-slider-slick-slider/dist/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.rtl-slider').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.rtl-slider-nav'
+	});
+	$('.rtl-slider-nav').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		vertical: true,
+	   asNavFor: '.rtl-slider',
+	   centerMode: false,
+	   focusOnSelect: true,
+		prevArrow: ".thumb-prev",
+   	nextArrow: ".thumb-next",
+	});
+});
